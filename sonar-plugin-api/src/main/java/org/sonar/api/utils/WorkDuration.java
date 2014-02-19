7327e9e742bcd2d038afe73d23198bde62e2ca95
Julien Lancelot <julien.lancelot@sonarsource.com>
2014-02-19 14:54:30 +0100

    HOURS_IN_DAY property should not be read in the constructor

--- sonar-plugin-api/src/main/java/org/sonar/api/utils/WorkDuration.java
+++ sonar-plugin-api/src/main/java/org/sonar/api/utils/WorkDuration.java
@@ -25,7 +25,6 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 
 import javax.annotation.Nullable;
-
 import java.io.Serializable;
 
 /**
